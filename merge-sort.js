f20904fa9b7a096714b3d4e96c968ac551d392ac
Kat M <kmunson13@gmail.com>
2022-11-08 09:12:08 -0600

    Add mergeSort function

--- merge-sort.js
+++ merge-sort.js
@@ -1,9 +1,19 @@
 function mergeSort(array) {
-    let leftSide = []
-    let rightSide = []
-    half = array.length
+    if (array.length <=1) {return array}
+    let half = Math.floor(array.length/2)
+    let splitRight = mergeSort(array.slice(half))
+    let splitLeft = mergeSort(array.slice(0, half))
+
+    return merge(splitLeft, splitRight)
 }
 
+function merge(left, right) {
+    let sorted = []
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {sorted.push(left.shift())}
+    }
+}
 
-
-[4, 3, 2, 5, 1, 6]
+array = [4, 3, 2, 5, 1, 6]
+mergeSort(array)
+console.log(mergeSort(array))
